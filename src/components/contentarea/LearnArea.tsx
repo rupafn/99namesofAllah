38a0f7384377cd787e80e011d196a9e8797a1612
rupafn <rupafn@gmail.com>
2023-04-21 01:54:45 +0800

    designed ui

--- src/components/contentarea/LearnArea.tsx
+++ src/components/contentarea/LearnArea.tsx
@@ -2,12 +2,15 @@ import { Container, Button } from 'react-bootstrap';
 
 export default function LearnArea() {
     return (
-      <div>
-          <Container> 
-            <h1>Rahman</h1>
-            <p>Beneficient</p>
-            <Button>Previous</Button>
-            <Button>Next</Button>
+      <div className="learn-area">
+          <Container className="text-center" fluid="true"> 
+            <h1 className="learn-title">Rahman</h1>
+            <p className="learn-meaning">Beneficient</p>
+            <div className="learn-control">
+                <Button className="learn-prev-btn btn-sm">Previous</Button>
+                <Button className="learn-next-btn btn-sm">Next</Button>
+            </div>
+            
           </Container>
       </div>
     )
